cbfe949cf71f0107f3a5b7c4716da2be25d457aa
Jorge Garcia <jorgegarcia.java@gmail.com>
2022-05-09 16:36:42 +0200

    Update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../services/user.service";
 import {User} from "../../models/models/user.model";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-profile',
@@ -10,20 +11,25 @@ import {User} from "../../models/models/user.model";
 })
 export class ProfileComponent implements OnInit {
 
-  public changeForm: FormGroup;
+  private _changeForm: FormGroup;
+  private _formSubmitted = false;
+
+  get changeForm(){
+    return this._changeForm;
+  }
 
   constructor(private fb: FormBuilder,
               private userService: UserService) {
     const user: User = userService.userActive;
     const date = user.birthDate.toString().split('T')[0];
-    this.changeForm = this.fb.group({
-      name: [user.name],
-      lastName: [user.lastName],
-      birthDate: [date],
+    this._changeForm = this.fb.group({
+      name: [user.name,[Validators.required, Validators.minLength(5)]],
+      lastName: [user.lastName, [Validators.required, Validators.minLength(5)]],
+      birthDate: [date, [Validators.required]],
       phone: [user.phone],
-      province: [user.province],
-      city: [user.city],
-      address: [user.address]
+      province: [user.province, [Validators.required, Validators.minLength(2)]],
+      city: [user.city, [Validators.required, Validators.minLength(2)]],
+      address: [user.address, [Validators.required, Validators.minLength(2)]]
     });
   }
 
@@ -31,10 +37,46 @@ export class ProfileComponent implements OnInit {
   }
 
   save() {
-    console.log(this.changeForm.value);
-    this.userService.updateUser(this.changeForm.value).subscribe(resp => {
-      console.log(resp);
+    Swal.fire({
+      title: '¿Quieres guardar los cambios?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, guardar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this._confirmSave()
+      }
+    });
+  }
+
+  private _confirmSave(){
+    this._formSubmitted = true;
+
+    if(!this._changeForm.valid){
+      return;
+    }
+
+    this.userService.updateUser(this.changeForm.value).subscribe({
+      next: _ => {
+        Swal.fire('Saved!', '', 'success')
+      },
+      error: _ => {
+        Swal.fire('Cambios no guardados', '', 'info')
+      }
     })
+  }
+
+  fieldNoValid(field: string):boolean {
+
+    if(this._changeForm.get(field)!.invalid && this._formSubmitted){
+      return true;
+    }else{
+      return false;
+    }
+
   }
 
   openModalImg() {
